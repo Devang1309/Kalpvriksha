4ad22e102fff6d5e554bff1bb3bf3b9285e41013
Devang1309 <devangchaturvedi13@gmail.com>
2024-12-20 18:33:47 +0530

    Completed Calculator code

--- calculator_updated.c
+++ calculator_updated.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int calculate(char *exp){
-    int current = 0, res = 0, temp = 0;
-    char op = '+'; 
-
-    int i;
-    for(i = 0; exp[i] != '\0'; i++){
-        char c = exp[i];
-#include <stdio.h>
-#include <ctype.h>
-
 void calculate(char *exp){
 	int j; int count=0;             //This is to count the size of exp
 	for(j=0; exp[j]!='\0'; j++){
@@ -105,52 +95,5 @@ void main(){
     fgets(exp, sizeof(exp), stdin);
 
     calculate(exp);
-}
-
-        if(isdigit(c)){
-            current = current * 10 + (c - '0');  
-        }
-
-        if(!isdigit(c) && c != ' ' || exp[i + 1] == '\0'){
-            if (op == '+'){
-                res += temp;  
-                temp = current;  
-            } 
-			else if (op == '-'){
-                res += temp;  
-                temp = -current;  
-            } 
-			else if (op == '*'){
-                temp *= current;  
-            } 
-	else if (op == '/'){
-            	if(current==0){
-            		printf("Error: Division by Zero\n");
-            		return;
-				}
-                temp /= current;  
-            }
-        else{
-            printf("Error: Invalid Expression\n");
-            return;
-	}
-
-            op = c;  
-            current = 0;  
-        }
-    }
-
-    res += temp;  
-    printf("%d\n", res);
-}
-
-void main(){
-    char exp[100];
-    
-    printf("Enter a mathematical expression: ");
-    fgets(exp, sizeof(exp), stdin);
-
-    calculate(exp);
-    return 0;
 }
 
