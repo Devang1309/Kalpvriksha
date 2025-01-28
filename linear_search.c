4e7bde67c6bd1d5eb0ec11301d4059325224ee7d
Devang Chaturvedi <devang.c@intimetec.com>
2025-01-28 14:41:24 +0530

    Linear Search Implementation

---
+++ linear_search.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isnumber(char ch){
+    if(ch>='0' && ch<='9'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int linear_search(int* array, int length, int target){
+    for(int i=0; i<length; i++){
+        if(array[i]==target){
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(){
+    int* array = (int*)calloc(1000,sizeof(int));
+    int data;
+
+    printf("Enter the elements of array: \n");
+    char expression[1000];
+    fgets(expression, sizeof(expression), stdin);
+    expression[strcspn(expression, "\n")] = '\0';
+
+    int i=0; int negative = 0;
+    int index = 0;
+    while(expression[i]!='\0'){
+        if(isnumber(expression[i])){
+            array[index] = array[index]*10 + (expression[i]-'0');
+        }
+        
+        if(expression[i]==' ' || expression[i]=='\n'){
+            if(negative==1){
+                array[index] = array[index]*(-1);
+                negative = 0;
+            }
+            index++;
+        }
+        
+        if(expression[i]=='-'){
+            negative = 1;
+        }
+        
+        i++;
+    }
+
+    int length = index+1;
+    if(index==0){
+        printf("Enter a valid input!");
+        return 0;
+    }
+
+    int target;
+    printf("Enter the target to search: \n");
+    scanf("%d", &target);
+
+    int found = linear_search(array, length, target);
+
+    if(found == 1){
+        printf("Target is present");
+    }
+    else{
+        printf("Target is not present");
+    }
+
+    free(array);
+
+    return 0;
+}
